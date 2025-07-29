76774af6c63681b2e4d0bd5af1955eacc2a1da49
Jeeva0111 <146181258+Jeeva0111@users.noreply.github.com>
2025-07-29 08:21:50 +0530

    Add files via upload

---
+++ Passenger.java
@@ -0,0 +1,93 @@
+package Week5;
+
+import java.time.LocalDate;
+enum presource{
+	CHENNAI,
+	COIMBATORE,
+	MADURAI,
+	TRICHY,
+	PUDUCHERRY;
+	
+}
+enum predestination{
+	BANGALORE,
+	MUMBAI,
+	DELHI,
+	KOCHI;
+}
+
+enum Berth{
+    UPPER,
+    MIDDLE,
+    LOWER
+}
+public class Passenger {
+
+    private int id;
+
+    private String name;
+
+    private Berth prefBerth;
+   
+    private Berth allocatedBerth;
+
+    private LocalDate bkdDate = LocalDate.now();
+
+    public Passenger(int id,String name,Berth prefBerth){
+        this.id = id;
+        this.name=name;
+        this.prefBerth=prefBerth;
+        
+    }
+    @Override
+    public String toString() {
+        return "Passenger{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", prefBerth=" + prefBerth +
+                ", allocatedBerth=" + allocatedBerth +
+                ", bkdDate=" + bkdDate +
+                
+                '}';
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Berth getPrefBerth() {
+        return prefBerth;
+    }
+
+    public void setPrefBerth(Berth prefBerth) {
+        this.prefBerth = prefBerth;
+    }
+
+    public Berth getAllocatedBerth() {
+        return allocatedBerth;
+    }
+
+    public void setAllocatedBerth(Berth allocatedBerth) {
+        this.allocatedBerth = allocatedBerth;
+    }
+
+    public LocalDate getBkdDate() {
+        return bkdDate;
+    }
+
+    public void setBkdDate(LocalDate bkdDate) {
+        this.bkdDate = bkdDate;
+    }
+}
